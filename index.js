30e4480e31d15ac5c56ff1a73469b7706fbe3894
carlos <carloscarmonaalcantara@gmail.com>
2017-04-28 11:39:41 +0200

    Simplify executor development

--- index.js
+++ index.js
@@ -11,73 +11,59 @@ class s3Executor extends Execution {
     super(process);
   }
 
-  exec() {
+  exec(params) {
     var _this = this;
 
-    return new Promise(function (resolve, reject) {
-      _this.getValues()
-        .then((res) => {
+    var awsS3Config = {
+      apiVersion: params.apiVersion,
+      accessKeyId: params.accessKeyId,
+      secretAccessKey: params.secretAccessKey,
+      bucket: params.bucket,
+      method: params.method,
+      region: params.region
+    };
 
-          var awsS3Config = {
-            apiVersion: res.apiVersion,
-            accessKeyId: res.accessKeyId,
-            secretAccessKey: res.secretAccessKey,
-            bucket: res.bucket,
-            method: res.method,
-            region: res.region
-          };
+    var s3 = new AWS.S3(awsS3Config);
 
-          var s3 = new AWS.S3(awsS3Config);
+    if (params.method === 'upload') {
 
-          if (res.method === 'upload') {
+      // call S3 to retrieve upload file to specified bucket
+      var uploadParams = {Bucket: params.bucket, Key: '', Body: ''};
+      var file_name = params.remote_file || path.basename(params.local_file);
 
-            // call S3 to retrieve upload file to specified bucket
-            var uploadParams = {Bucket: res.bucket, Key: '', Body: ''};
-            var file_name = res.remote_file || path.basename(res.local_file);
+      var fileStream = fs.createReadStream(params.local_file);
+      fileStream.on('error', function (err) {
+        _this.logger.log('error', 'S3 upload reading file Error', params.local_file, err);
+      });
 
-            var fileStream = fs.createReadStream(res.local_file);
-            fileStream.on('error', function (err) {
-              _this.logger.log('error', 'S3 upload reading file Error', res.local_file, err);
-            });
+      uploadParams.Body = fileStream;
+      uploadParams.Key = file_name;
 
-            uploadParams.Body = fileStream;
-            uploadParams.Key = file_name;
-
-            s3.upload(uploadParams, function (err, data) {
-              if (err) {
-                var endOptions = {
-                  end: 'error',
-                  messageLog: `S3 upload file Error: ${err}`,
-                  execute_err_return: `S3 upload file Error: ${err}`
-                };
-                _this.end(endOptions, resolve, reject);
-              }
-              else {
-                var endOptions = {
-                  end: 'end',
-                  execute_return: JSON.stringify(data)
-                };
-                _this.end(endOptions, resolve, reject);
-              }
-            });
-          } else {
-            var endOptions = {
-              end: 'error',
-              messageLog: `S3 method not accepted: ${method}`,
-              execute_err_return: `S3 method not accepted: ${method}`
-            };
-            _this.end(endOptions, resolve, reject);
-          }
-        })
-        .catch((err) => {
+      s3.upload(uploadParams, function (err, data) {
+        if (err) {
           var endOptions = {
             end: 'error',
-            messageLog: `S3 Error getValues: ${err}`,
-            execute_err_return: `S3 Error getValues: ${err}`
+            messageLog: `S3 upload file Error: ${err}`,
+            execute_err_return: `S3 upload file Error: ${err}`
           };
-          _this.end(endOptions, resolve, reject);
-        });
-    });
+          _this.end(endOptions);
+        }
+        else {
+          var endOptions = {
+            end: 'end',
+            execute_return: JSON.stringify(data)
+          };
+          _this.end(endOptions);
+        }
+      });
+    } else {
+      var endOptions = {
+        end: 'error',
+        messageLog: `S3 method not accepted: ${method}`,
+        execute_err_return: `S3 method not accepted: ${method}`
+      };
+      _this.end(endOptions);
+    }
   }
 }
 
